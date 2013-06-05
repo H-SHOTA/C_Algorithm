534d506f96a876f551173b6c5a486a4f1719fdfb
a1148sh <a1148sh@aiit.ac.jp>
2013-06-05 22:34:30 +0900

    連結リスト（挿入、削除実装）

--- C_Algorithm/linkedlist.cpp
+++ C_Algorithm/linkedlist.cpp
@@ -9,78 +9,93 @@
 struct CELL{
 	struct CELL *next;
 	int value;
-} header;
+} *header;
 
 // 削除方式
 enum DELETE_TYPE{
 	FRONT = 0,	//先頭削除
-	REAR,		//末尾削除
+	REAR,
 	SPECIFIC	//特定の値の削除
 };
 
 // 挿入
 bool Insert(int value)
 {
-	CELL *newVal, *now, *prev;
-	// 新要素生成
-	if( (newVal = (CELL*)malloc(sizeof(CELL))) == NULL )
+	// リストが空の場合はヘッダに追加
+	if( header == NULL )
 	{
-		puts("memory error!");
-		return false;
+		header = (CELL*)calloc(1, sizeof(CELL));
+		header->value = value;
+		return true;
 	}
-	newVal->value = value;
-
-	// 末尾移動
-	now = header.next;
-	prev = &header;
-	while( now != NULL )
+	// 末尾に代入
+	CELL *pos = header, *prev;
+	while( pos != NULL )
 	{
-		prev = now;
-		now = now->next;
+		prev = pos;
+		pos = pos->next;
 	}
-
-	// 値の代入
-	newVal->next  = now;
-	newVal->value = value;
-	prev->next = newVal;
+	prev->next = (CELL*)calloc(1, sizeof(CELL));
+	prev->next->value = value;
 	return true;
 }
 
 // 削除
 bool Delete(int value, DELETE_TYPE _type)
 {
+	if(&header == NULL)
+	{
+		printf("list is empty\n");
+		return false;
+	}
+	
 	switch(_type)
 	{
 	case FRONT:
 		{
-			if(&header == NULL)
-			{
-				printf("list is empty\n");
-				return false;
-			}
-
-			CELL temp = header;
-			header = *temp.next;
-			free(&temp);
+			CELL* p = header;
+			header = p->next;
+			free(p);
 		}
+		break;
 	case REAR:
 		{
-
+			CELL *pos = header, *prev = NULL;
+			while( pos->next != NULL )
+			{
+				prev = pos;
+				pos = pos->next;
+			}
+			prev->next = pos->next;
+			free(pos);
 		}
+		break;
 	case SPECIFIC:
 		{
-
+			CELL *pos = header, *prev = NULL;
+			while( pos->next != NULL )
+			{
+				if(pos->value == value)
+				{
+					break;
+				}
+				prev = pos;
+				pos = pos->next;
+			}
+			if( prev != NULL )
+			{
+				prev->next = pos->next;
+				free(pos);
+			}
+			else
+			{
+				CELL* p = header;
+				header = p->next;
+				free(p);
+			}
 		}
+		break;
 	default:
 		break;
 	}
-}
-
-void main()
-{
-	Insert(5);
-	Insert(4);
-	Insert(3);
-	Delete(0, FRONT);
-	return;
 }
